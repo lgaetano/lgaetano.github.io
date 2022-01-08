6ee65456db04a9e1fb0c429af57a8f406f416a00
Lia Gaetano <lgaetano19@gmail.com>
2022-01-08 10:42:58 -0800

    Add files via upload

---
+++ src/components/ProjectList.js
@@ -0,0 +1,25 @@
+import Project from './Project.js';
+import './ProjectList.css';
+
+const ProjectList = ({ id, projects }) => {
+
+    const getProjectList = (projects) => {
+        return projects.map((project, index) => {
+            return <Project 
+                    key={index} 
+                    title={project.title}
+                    link={project.link}
+                    />
+        });
+    };
+
+    return (
+        <section className='projectList'>
+            <ul className='flex-grid'>
+                {getProjectList(projects)}
+            </ul>
+        </section>
+    )
+};
+
+export default ProjectList;
